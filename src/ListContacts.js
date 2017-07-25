e65f41f9430bd989130f77f34055bcc76821561c
nora humpage <nora@noras-MacBook-Pro-2.local>
2017-07-25 14:18:14 +0200

    contacts page looks good

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,12 +1,24 @@
 import React, { Component} from 'react'
 
+
+// {person.name}
 class ListContacts extends Component {
 	render(){ 
 		console.log("this.props" , this.props)
 		return (
 			<ol className='contact-list'>
-
-
+				{this.props.contacts.map(person =>
+				 ( 
+				 	<li key={person.id} className="contact-list-item">
+				 		<div className= "contact-avatar" style= {{
+				 			backgroundImage :`url(${person.avatarURL})`
+				 		}}></div>
+				 		<div className="contact-details">
+				 			<p>{ person.name }</p>
+				 			<p>{ person.email }</p>
+				 		</div>
+				 	</li>
+				 ))}
 			</ol>
 		)
 	}
